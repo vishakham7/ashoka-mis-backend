c576891f45aa9804b497033f1ab7c9e2b7a40dd8
kumarm5 <m.kumar@splendornet.com>
2018-11-20 16:09:41 +0530

    address field removed in validation

--- taiga-back/taiga/users/validators.py
+++ taiga-back/taiga/users/validators.py
@@ -64,7 +64,7 @@ class UserAdminValidator(UserValidator):
         # IMPORTANT: Maintain the UserSerializer Meta up to date
         # with this info (including here the email)
         fields = ("username", "full_name", "color", "bio", "lang",
-                  "theme", "timezone", "is_active", "email", "read_new_terms", "address")
+                  "theme", "timezone", "is_active", "email", "read_new_terms")
 
     def validate_read_new_terms(self, attrs, source):
         value = attrs[source]
