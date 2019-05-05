4cb08133a6f84ab7b462f1c71edb2a3d6d600df5
Vishakha <v.mote@splendornet.com>
2019-05-05 16:06:26 +0530

    added

--- taiga-back/taiga/projects/issues/api.py
+++ taiga-back/taiga/projects/issues/api.py
@@ -408,6 +408,7 @@ class IssueViewSet(
 
         project = get_object_or_404(Project, issues_csv_uuid=uuid)
         queryset = project.issues.filter(type__name=type, created_date__date__range=[start_date, end_date],status__name=status).order_by('ref')
+        print(queryset)
         data = services.issues_to_csv(project, queryset, type, status)
         csv_response = HttpResponse(data.getvalue(), content_type='application/csv; charset=utf-8')
         csv_response['Content-Disposition'] = 'attachment; filename="issues.csv"'
