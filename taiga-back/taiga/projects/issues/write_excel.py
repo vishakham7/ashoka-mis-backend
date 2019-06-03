5d25f432f0fe438ad8a77a0374a8e03469b1d4f9
Vishakha <v.mote@splendornet.com>
2019-06-03 14:57:07 +0530

    pdf

--- taiga-back/taiga/projects/issues/write_excel.py
+++ taiga-back/taiga/projects/issues/write_excel.py
@@ -1547,7 +1547,7 @@ def write_excel(project, queryset, type, status,start_date, end_date,asset, perf
         #  
         pd.set_option('display.max_colwidth', 500)   # FOR TABLE <th>
 
-        html = new.to_html(escape=False,header=False,border="0.5").replace('&lt;','<').replace('&gt;', '>')
+        html = new.to_html(border="0.5").replace('&lt;','<').replace('&gt;', '>')
         pisa_context = pisa.CreatePDF(html)
         response = pisa_context.dest.getvalue()
         return html
